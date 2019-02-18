c60d736ee178b2e92f3a3a9a06296965b1bf058b
= <svelingker@utexas.edu>
2019-02-18 06:22:28 -0600

    Format basic UI layout

--- src/index.js
+++ src/index.js
@@ -1,20 +1,75 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ons from 'onsenui';
-import { Page } from 'react-onsenui';
+import { Page, Row, Col, Input, Button, List, ListItem } from 'react-onsenui';
 
-/**
-    @class Backbone of app: side menu + navigation routing
-*/
 class Skeleton extends React.Component {
-    /**
-        Main frame for entire app.
-        Creates a menu with a navigator inside.
-        Menu is controlled by one fn.
-        Navigator controls page content.
-    */
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            query: '',
+            movies: [
+                {
+                    name: 'The Avengers',
+                    img:
+                        'https://nerdist.com/wp-content/uploads/2018/05/landscape-1500632962-avengers-assemble.jpg',
+                    rating: 10,
+                    desc: 'Badass superheroes',
+                },
+            ],
+        };
+    }
+
+    renderMovie = movie => {
+        return (
+            <ListItem expandable tappable>
+                <div className="left">
+                    <img
+                        src={movie.img}
+                        alt={movie.name}
+                        style={{
+                            height: '100px',
+                            width: '100px',
+                        }}
+                    />
+                </div>
+                <div className="center">{movie.name}</div>
+                <div className="expandable-content">{movie.desc}</div>
+            </ListItem>
+        );
+    };
+
+    search = () => {
+        //
+    };
+
     render = () => {
-        return <Page>Hello!</Page>;
+        return (
+            <Page>
+                <Row style={{ margin: '10px' }}>
+                    <Col style={{ margin: 'auto', textAlign: 'center' }}>
+                        <Input
+                            value={this.state.query}
+                            onChange={e =>
+                                this.setState({ query: e.target.value })
+                            }
+                            placeholder="Find a movie!"
+                        />
+                        <Button
+                            onClick={this.search}
+                            style={{ margin: '15px' }}>
+                            Search
+                        </Button>
+                    </Col>
+                </Row>
+
+                <List
+                    dataSource={this.state.movies}
+                    renderRow={this.renderMovie}
+                />
+            </Page>
+        );
     };
 }
 
